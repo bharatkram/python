04c2a924911537102c46702089b7e89c6828d384
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-05 16:01:34 +0530

    Parsing of the dependants done.

--- Command line parser/CustomCommandLine.py
+++ Command line parser/CustomCommandLine.py
@@ -35,6 +35,7 @@ def singleCommands(command):
         print("", *os.listdir(), "", sep="\n")
 
 
+# function to sort the inputs as they are inserted.
 def insertionSort(inpList, inp):
     inpList.append(inp)
     if len(inpList) == 1:
@@ -49,6 +50,7 @@ def insertionSort(inpList, inp):
     return inpList
 
 
+# function to take inputs from user that are to be sorted.
 def takeInputs():
     inpList = []
     while True:
@@ -58,25 +60,95 @@ def takeInputs():
             if inp.strip() == "":
                 continue
             inpList = insertionSort(inpList, inp)
+        # break the loop if the user inputs ctrl+c.
         except KeyboardInterrupt:
             print("", *inpList, sep="\n")
             return
 
 
+# function to sort the contents in file and output them to file or panel as per users input.
+def sortContents(fileName, reverse, output):
+    print("here")
+    return
+
+
+# function to execute each flag and parse the command.
+def sortWithDependants(command, dependants):
+    # variable to know if the sorting is to be done in the reverse order.
+    reverse = False
+    # variable to know if the sorted order is to be saved in a file or output to screen.
+    output = False
+    # variable to get the filename whose contents are to be sorted.
+    fromFile = ""
+
+    # tuple to store the available single letter flags.
+    singleLetterFlags = ("-r", "-o")
+    # tuple to store the available double dash flags.
+    doubleDashFlags = ("--reverse", "--output")
+    while dependants != "":
+        # line to separate each word in the command.
+        flag, _, dependants = dependants.partition(" ")
+
+        # to know if the word in the command is a flag.
+        if flag[0] == "-":
+            # to know if the double dash function is recognised.
+            if flag[1] == "-" and flag.split("=")[0] in doubleDashFlags:
+                pass
+            # to know if the single letter function if recognised.
+            elif flag[:2] in singleLetterFlags:
+                pass
+            # if the flag is not recognised.
+            else:
+                print(f"{flag} not recognised.")
+                return
+
+        if flag == "-r" or flag == "--reverse":
+            reverse = True
+        elif flag[:2] == "-o":
+            if len(flag) != 2:
+                output = flag[2:]
+            else:
+                output, _, dependants = dependants.partition(" ")
+        elif flag[:8] == "--output":
+            flag = flag.split("=")
+            if len(flag) == 1:
+                output, _, dependants = dependants.partition(" ")
+            else:
+                output = flag[1]
+        else:
+            if fromFile == "":
+                fromFile = flag
+            else:
+                print("Given two file names. Needed only one.")
+                return
+
+    # to know the file name is given from where the input is to be read.
+    if fromFile == "":
+        print("Required a filename to read from.")
+        return
+    # to know if the file exists.
+    if not os.path.isfile(fromFile):
+        print("File not found.")
+        return
+    # to perform the task given.
+    sortContents(fromFile, reverse, output)
+
+
+# to segregate if the command depends on a file or user inputs the values.
 def sortCommand(command, dependants):
     if not dependants:
         takeInputs()
         return
-
-    print("sort called")
-    return
+    sortWithDependants(command, dependants)
 
 
+# to perform the read operation.
 def catCommand(command, dependants):
     print("cat called")
     return
 
 
+# to call the function according to the command.
 def dependantCommands(command, dependants):
     commands = {"sort": sortCommand, "cat": catCommand}
     commands[command](command, dependants)
